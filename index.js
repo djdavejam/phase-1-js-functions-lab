7ad491a367b842b4fb21d8a42525ae73316d08d8
David James <davidanthoneyjames@gmail.com>
2025-07-12 09:18:14 -0400

    This is the Functions in Javascript Lab updated

--- index.js
+++ index.js
@@ -4,4 +4,32 @@
 
 
 // This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };\
+
+
+function calculateTax(amount) {
+    return amount * 0.10; 
+}
+
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+
+function findMaximum(num1, num2){
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+function isPalindrome(word) {
+    const cleanWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedWord = cleanWord.split('').reverse().join('');
+    return cleanWord === reversedWord;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountAmount = (originalPrice * discountPercentage) / 100;
+    return originalPrice - discountAmount;
+}
